345ffabc1070d300a4e4ba5fda193da73c6b5442
Islom_the_dev <polzovatelit1@gmail.com>
2024-08-15 13:23:53 +0300

    jsCalculator

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-let question = +prompt("Введите текущее время!")
+let question = +prompt("Введите число")
+let sign = prompt("Введите знак")
+let question_2 = +prompt("Введите число")
 
-switch (question) {
-  case 6:
-  case 7:
-  case 8:
-  case 9:
-  case 10:
-  case 11:
-  case 12:
-    alert("Доброе утро Вавилон!!!")
-    console.log("Доброе утро Вавилон!!!");
-    
+switch (sign) {
+  case "+":
+    result = question + question_2;
     break;
-  case 12:
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
-    alert("*Звук включения Windows XP*")
-    console.log("*Звук включения Windows XP*");
-    break;
-  case 18:
-  case 19:
-  case 20:
-  case 21:
-  case 22:
-  case 23:
-  case 24:
-    alert("Добрый вечер !!!")
-    console.log("Добрый вечер !!!");
-    break;
+  case "-":
+    result = question - question_2;
+
+  case "*":
+    result = question * question_2;
+
+  case "/":
+    result = question / question_2
+
+
+
 
   default:
-    alert("*Звук выключения Windows XP*")
-    console.log("*Звук выключения Windows XP*");
+    alert("Это че за цифры такие бархатнные!")
     break;
 }
+alert(`Результат ${result} `)
 
 
